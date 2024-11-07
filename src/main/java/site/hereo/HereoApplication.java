5162504872f6b13627d0812999422fb0a05818bf
2eunjung <12103207@gmail.com>
2024-11-07 18:35:49 +0900

    first commit - 프로젝트 환경설정

---
+++ src/main/java/site/hereo/HereoApplication.java
@@ -0,0 +1,13 @@
+package site.hereo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HereoApplication.class, args);
+    }
+
+}
